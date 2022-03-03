b2dcf6785ee9695e7fcc61fccd6d8c9bfdbe6040
Holly Christopher <holly_christopher@hotmail.com>
2022-03-03 17:30:02 +0000

    third commit

--- app.js
+++ app.js
@@ -5,12 +5,13 @@ document.addEventListener("keydown", (event) => {
     let numKey = document.getElementById("keyDisplay");
     let numWhich = document.getElementById("whichDisplay");
 
-    let keyDown = event.key;
     let keyCode = event.code;
+    let keyDown = event.key;
     let keyWhich = event.which;
 
-    numDisplay.innerHTML = `<p class ="pressedBtn"> ${keyDown } WAS PRESSED</p>`
-    numKey.innerHTML = `<p class ="pressedBtn"> ${keyDown } WAS PRESSED</p>`
+    numDisplay.innerHTML = `<p class ="pressedBtn"> ${keyDown } </p>`
     numCode.innerHTML = `<p class ="pressedBtn"> ${keyCode } WAS PRESSED</p>`
+    numKey.innerHTML = `<p class ="pressedBtn"> ${keyDown } WAS PRESSED</p>`
     numWhich.innerHTML = `<p class ="pressedBtn"> ${keyWhich } WAS PRESSED</p>`
+    
 })
